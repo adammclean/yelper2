735ac0ed994042aa828384f9d4c8786aba5a7e7e
Adam McLean <adam.mclean1@gmail.com>
2015-05-15 09:21:48 -0700

    add devise gem, views and user model

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'carrierwave', '~> 0.10.0'
 gem 'figaro', '~> 1.1.1'
 gem 'fog', '~> 1.30.0'
 gem 'mini_magick', '~> 4.2.4'
+gem 'devise', '~> 3.4.1'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -39,4 +40,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Required for Devise gem; if custom host changes we need to reflect change below
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+
 end
