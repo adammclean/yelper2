60a25cf23867d7c18d60d168d9ebe0986a715334
Adam McLean <adam.mclean1@gmail.com>
2015-05-22 15:27:50 -0700

    remove excess reviews controller index and show actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,14 +4,11 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   # GET /reviews
   # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
+
 
   # GET /reviews/1
   # GET /reviews/1.json
-  def show
-  end
+
 
   # GET /reviews/new
   def new
